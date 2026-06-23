ba188bb56430d0178bf117d9e490146ca5fe6359
Shohruhmirzo Usmonov <usmonovshohruxmirzo@gmail.com>
2026-06-23 15:28:38 +0500

    feat(data): add repos data included name, description and repo url

---
+++ src/data/repos.ts
@@ -0,0 +1,33 @@
+interface IRepo {
+  name: string;
+  description: string;
+  repo_url: string;
+}
+const repos: IRepo[] = [
+  {
+    name: 'NitroVim',
+    description: 'Just install. Start coding. Done.',
+    repo_url: 'https://github.com/NitroBrain/nitrovim',
+  },
+  {
+    name: 'NitroHttp',
+    description: 'Lightweight API testing tool ',
+    repo_url: 'https://github.com/NitroBrain/nitrohttp',
+  },
+  {
+    name: 'NitroShell',
+    description: 'Fast, sleek, and modern terminal.',
+    repo_url: 'https://github.com/NitroBrain/nitroshell',
+  },
+  {
+    name: 'NitroOS',
+    description: 'Hyprland-first developer OS',
+    repo_url: 'https://github.com/Nitro-OS/NitroHypr',
+  },
+  {
+    name: 'NitroBrain Labs',
+    description: 'Hyprland-first developer OS',
+    repo_url: 'https://labs.nitrobrain.dev/dashboard',
+  }
+];
+export default repos;
